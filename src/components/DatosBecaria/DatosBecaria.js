2a3e5c7559e5066003e0ba888e85696a4f5613a6
fragata-adrian <fragataadrian99@gmail.com>
2021-09-21 16:38:58 -0300

    termine el container con el nombre d ela becaria y los botones

--- src/components/DatosBecaria/DatosBecaria.js
+++ src/components/DatosBecaria/DatosBecaria.js
@@ -3,6 +3,12 @@ import Dato from './Dato'
 import React from 'react'
 import Typography from '@mui/material/Typography';
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const useStyles = makeStyles((theme) => ({
     image: {
@@ -42,27 +48,44 @@ export default function DatosBecaria() {
 
     return (
         <Container maxWidth="md">
-            <Container id='nombreBecaria'> 
-              <Typography variant='h5'>{datos.nombre}</Typography>
-              <Typography variant='h5'>{datos.apellido}</Typography>
+            <Container id='nombreBecaria'>
+              <Grid container>
+                <Grid container xs={8}>
+                  <Box>
+                    <Typography variant='h5'>{datos.apellido}</Typography>
+                    <Typography variant='h5'>{datos.nombre}</Typography>
+                  </Box>
+                </Grid>
+                <Grid container xs={4}>
+                  <Box>
+                    <IconButton>
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton>
+                      <PersonAddAlt1Icon />
+                    </IconButton>
+                    <IconButton>
+                      <CancelIcon />
+                    </IconButton>
+                  </Box>
+                </Grid>
+              </Grid>
             </Container>
             <Container id='datosGenerales' >
-                <Container id='datosGeneralesUp' disableGutters>
-                    <Container id='DatosPersonales' disableGutters>
-                        <img src={datos.fotoURL} className={classes.image} alt='fotoBecaria'/>
-                        <Dato title='DNI' value={datos.dni} mail=''/>
-                        <Dato title='Nombre' value={datos.nombre}/>
-                        <Dato title='Apellido' value={datos.apellido}/>
-                        
+              <Container id='datosGeneralesUp' disableGutters>
+                  <Container id='DatosPersonales' disableGutters>
+                      <img src={datos.fotoURL} className={classes.image} alt='fotoBecaria'/>
+                      <Dato title='DNI' value={datos.dni} mail=''/>
+                      <Dato title='Fecha de nacimiento' value={datos.fechaNacimiento}/>
+                      <Dato title='Localidad' value={datos.ciudad + ', ' + datos.provincia + ', ' + datos.pais}/>
+                  </Container>
+                  <Container id='datosContCar' disableGutters>
+                   
+                  </Container>
+              </Container>
+              <Container id='datosGeneralesBottom'>
 
-                    </Container>
-                    <Container id='datosContCar' disableGutters>
-                     
-                    </Container>
-                </Container>
-                <Container id='datosGeneralesBottom'>
-
-                </Container>
+              </Container>
             </Container>
         </Container>
     )
