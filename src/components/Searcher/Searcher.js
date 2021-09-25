d16c65eda2fa0b564c9c1370758bc1ab5dd753e7
Roman <r_af_95@hotmail.com>
2021-09-25 16:22:27 -0300

    cambiada tabla

--- src/components/Searcher/Searcher.js
+++ src/components/Searcher/Searcher.js
@@ -39,27 +39,33 @@ const columns = [
         params.getValue(params.id, 'lastName') || ''
       }`,*/
   },
+  {field: 'career',headerName: 'Carrera',width: 150,editable: true,},
+  {field: 'announcement',headerName: 'Convocatoria',type: 'number',width: 180,editable: true,},
+  {field: 'weighing',headerName: 'Ponderación',type: 'number',width: 180,editable: true,},
+  {field: 'tutor',headerName: 'Tutor',width: 150,editable: true,}
+
 ];
 
 const rows = [
-  { id: 1, lastName: 'Fabiola', firstName: 'Suarez', dni: "42.312.342" ,age: 35 , state: 'Aceptada'},
-  { id: 2, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada'},
-  { id: 3, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 21 , state: 'En espera'},
-  { id: 4, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 35 , state: 'Rechazada'},
-  { id: 5, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Analizandose'},
-  { id: 7, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'En espera'},
-  { id: 8, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada'},
-  { id: 9, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada'},
-  { id: 10, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Aceptada'},
-  { id: 11, lastName: 'Fabiola', firstName: 'Suarez', dni: "42.312.342" ,age: 35 , state: 'Aceptada'},
-  { id: 12, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada'},
-  { id: 13, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 21 , state: 'Rechazada'},
-  { id: 14, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 35 , state: 'Rechazada'},
-  { id: 15, lastName: 'Carol', firstName: 'Suris', dni: "37.659.881" ,age: 25 , state: 'Analizandose'},
-  { id: 17, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Aceptada'},
-  { id: 18, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Aceptada'},
-  { id: 19, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'En espera'},
-  { id: 20, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada'},
+  { id: 1, lastName: 'Fabiola', firstName: 'Suarez', dni: "42.312.342" ,age: 35 , state: 'Aceptada',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 2, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada',career: " Tec. en Informática",announcement: 2020,weighing: 2021,tutor: "Sabrina Schiaretti"},
+  { id: 3, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 21 , state: 'En espera',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Alicia Ramos"},
+  { id: 4, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 35 , state: 'Rechazada',career: " Tec. en Informática",announcement: 2020,weighing: 2021,tutor: "Alicia Ramos"},
+  { id: 5, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Analizandose',career: " Tec. en Informática",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 7, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'En espera',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 8, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada',career: " Tec. en Informática",announcement: 2020,weighing: 2021,tutor: "Alicia Ramos"},
+  { id: 9, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 10, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Aceptada',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 11, lastName: 'Fabiola', firstName: 'Suarez', dni: "42.312.342" ,age: 35 , state: 'Aceptada',career: " Tec. en Informática",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 12, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada',career: " Tec. en Informática",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 13, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 21 , state: 'Rechazada',career: " Tec. en Electrónica",announcement: 2020,weighing: 2021,tutor: "Alicia Ramos"},
+  { id: 14, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 35 , state: 'Rechazada',career: " Tec. en Informática",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 15, lastName: 'Carol', firstName: 'Suris', dni: "37.659.881" ,age: 25 , state: 'Analizandose',career: " Tec. en Informática",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 17, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Aceptada',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Alicia Ramos"},
+  { id: 18, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Aceptada',career: " Tec. en Informática",announcement: 2019,weighing: 2020,tutor: "Sabrina Schiaretti"},
+  { id: 19, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'En espera',career: " Tec. en Informática",announcement: 2020,weighing: 2021,tutor: "Alicia Ramos"},
+  { id: 20, lastName: 'Carol', firstName: 'Suris', dni: "12.345.678" ,age: 25 , state: 'Rechazada',career: " Tec. en Electrónica",announcement: 2019,weighing: 2020,tutor: "Alicia Ramos"}
+
 ];
 
 export default function Searcher() {
