341025db52a90dde28cbf4b5a28d92356ed0fae5
alberto leandro sanchez <beto.unahur@gmail.com>
2021-10-04 00:03:43 -0300

    pantalla edicion prehecha

--- src/components/DatosBecaria/Dato.js
+++ src/components/DatosBecaria/Dato.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Input, TextField, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import { makeStyles } from "@mui/styles";
 import EmailIcon from '@mui/icons-material/Email';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+import { BecariaContext } from '../../context/DatosBecariaContext';
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
@@ -27,27 +29,28 @@ const useStyles = makeStyles((theme) => ({
     
   }));
 
-export default function Dato({title, value,mail,cell,editable}) {
-
+export default function Dato({title, value,mail,cell,editable, name}) {
+    const {isEditable, updateBecariaState} = useContext(BecariaContext);
     const classes = useStyles();
     return (
         <div>
             <Box className={classes.Box}>  
                 <Typography sx={{}}  variant="h6" >{title}</Typography> 
             
-                <Box class={classes.spacer}/>  
+                <Box className={classes.spacer}/>  
                 
                 {
                     mail && <EmailIcon />
                 }
                 {
                     cell && <WhatsAppIcon />
-                }    
+                }
             </Box>
             <Box ml={'2px'}>
-                {editable?
-                <Input size='small' defaultValue={value}></Input>:
-                <Typography variant="body1">{value}</Typography>}
+                {isEditable?
+                <Input size='small' name={name} onBlur={updateBecariaState} defaultValue={value}></Input>:
+                <Typography variant="body1">{value}</Typography>
+                }
             </Box>
         </div>
     )
