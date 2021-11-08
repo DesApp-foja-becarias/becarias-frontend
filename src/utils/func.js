c5d274497171b65f77a89ba0e1166fcba5dd287f
alberto leandro sanchez <beto.unahur@gmail.com>
2021-11-08 19:24:26 -0300

    .

--- src/utils/func.js
+++ src/utils/func.js
@@ -1,5 +1,7 @@
 export const mappedDate = (date) => date.split('-')[2] + '/' + date.split('-')[1] + '/' + date.split('-')[0];
 
+
+
 export const someEmptyField = (obj) => {
     for (let key in obj) {
         if (obj[key] === '' || obj[key] === [] || obj[key] === null) {
