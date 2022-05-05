2bbcae79d8695a1bd8d79bf1385b73fa46402c73
alberto leandro sanchez <albertoleandrosanchez@hotmail.com>
2022-05-05 18:54:43 -0300

    changes

--- src/constants/searchedActivities.js
+++ src/constants/searchedActivities.js
@@ -2,7 +2,7 @@ export const columnsActivities = [
     { field: 'id', headerName: 'ID', width: 50, },
     { field: 'name', headerName: 'Nombre', width: 300,},
     { field: 'startDate', headerName: 'Fecha de inicio', width: 200,},
-    { field: 'endDate', headerName: 'Fecha de finalización', width:200 ,},
+    { field: 'endDate', headerName: 'Fecha de finalización estimada', width:200 ,},
     { field: 'validity',headerName: 'Vigencia', type: 'email', width: 180, },
     { field: 'link', headerName: 'Link', width: 150, 
         renderCell: (params) => {
